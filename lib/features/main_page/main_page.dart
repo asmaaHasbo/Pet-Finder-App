fb7461b721fad619ae4003b8a18440758f8b37ef
asmaa <elsaeeedasmaa@gmail.com>
2025-10-14 16:50:18 +0300

    feat: Implement main screen with bottom navigation bar

---
+++ lib/features/main_page/main_page.dart
@@ -0,0 +1,140 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pet_finder_app/core/themes/app_colors.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+     const Center(child: Text('Wishlist')),
+     const Center(child: Text('Wishlist')),
+     const Center(child: Text('Wishlist')),
+     const Center(child: Text('Wishlist')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.05),
+            blurRadius: 10,
+            offset: const Offset(0, -5),
+          ),
+        ],
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 16.w),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildNavItem(
+                index: 0,
+                icon: Icons.home_outlined,
+                activeIcon: Icons.home,
+              ),
+              _buildNavItem(
+                index: 1,
+                icon: Icons.favorite_border,
+                activeIcon: Icons.favorite,
+              ),
+              _buildNavItem(
+                index: 2,
+                icon: Icons.shopping_bag_outlined,
+                activeIcon: Icons.shopping_bag,
+              ),
+              _buildNavItem(
+                index: 3,
+                icon: Icons.account_balance_wallet_outlined,
+                activeIcon: Icons.account_balance_wallet,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem({
+    required int index,
+    required IconData icon,
+    required IconData activeIcon,
+  }) {
+    final isActive = _currentIndex == index;
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        padding: EdgeInsets.symmetric(
+          horizontal: (isActive ? 16 : 12),
+          vertical: 8,
+        ),
+
+        child: Icon(
+          isActive ? activeIcon : icon,
+          color: isActive ? Colors.transparent : AppColors.gray,
+          size: 24,
+        ),
+      ),
+    );
+  }
+}
+
+// class WishlistScreen extends StatelessWidget {
+//   const WishlistScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.white,
+//         elevation: 0,
+//         centerTitle: true,
+//         title: const Text('Wishlist'),
+//       ),
+//       body: const Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Icon(
+//               Icons.favorite_border,
+//               // size: ScreenUtil.setWidth(100),
+//               color: AppColors.gray,
+//             ),
+//             Text(
+//               'Your wishlist is empty',
+//             ),
+//             Text(
+//               'Add items you like to your wishlist',
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
