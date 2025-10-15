97830d9932b1a707399200b1230598a7717896b0
asmaa <elsaeeedasmaa@gmail.com>
2025-10-15 10:56:38 +0300

    dio factory class done  💪💃✅

---
+++ lib/core/networking/dio_factory.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+class DioFactory {
+  DioFactory._();
+
+  static Dio? dio;
+
+  static Dio getDio() {
+    Duration timeOut = const Duration(seconds: 30);
+
+    if (dio == null) {
+      dio = Dio();
+      dio!.options.connectTimeout = timeOut;
+      dio!.options.receiveTimeout = timeOut;
+
+      addDioHeader();
+
+      addDioInterceptor();
+      return dio!;
+    } else {
+      return dio!;
+    }
+  }
+
+  static addDioHeader() async {
+    dio!.options.headers = {'Accept': 'application/json'};
+  }
+
+  static void addDioInterceptor() {
+    dio?.interceptors.add(
+      PrettyDioLogger(
+        requestBody: true,
+        responseBody: true,
+        error: true,
+        requestHeader: true,
+        // responseHeader: true,
+      ),
+    );
+  }
+}
