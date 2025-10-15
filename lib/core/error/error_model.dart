f511f68a171c848112823258bf13a1d3aa165542
asmaa <elsaeeedasmaa@gmail.com>
2025-10-15 12:05:09 +0300

    creating error model done  💪💃✅

---
+++ lib/core/error/error_model.dart
@@ -0,0 +1,14 @@
+class ErrorModel {
+  final String message;
+  final int? statusCode;
+  final Map<String, dynamic>? data;
+
+  ErrorModel({
+    required this.message,
+    this.statusCode,
+    this.data,
+  });
+
+  @override
+  String toString() => message;
+}
